58c9e8e7d8b9530a90dc841fe00459621e3eca33
jacobcancode <jake.zierolf@gmail.com>
2024-12-11 09:19:27 -0800

    fix fix again

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
-
+// Use the CORS middleware
+app.use(cors({
+  origin: 'https://ahaanv19.github.io/LitConnect/' // Replace with your allowed origin
+}));
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello, World!');
