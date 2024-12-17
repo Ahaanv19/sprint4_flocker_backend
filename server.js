144fa9c33954308ba63ae53aa2f60bc1c6eacb07
jacobcancode <jake.zierolf@gmail.com>
2024-12-16 17:20:38 -0800

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'false');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
