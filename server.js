7a8a223d36527bb3dbc8f8fd93895696d89f174f
jacobcancode <jake.zierolf@gmail.com>
2024-12-16 19:19:05 -0800

    fix

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const port = 8887;
 
 // Use the CORS middleware to allow requests from your frontend
 app.use(cors({
-  origin: 'http://127.0.0.1:5000', // Specify the exact origin of your frontend
+  origin: 'http://127.0.0.1:4887', // Specify the exact origin of your frontend
   credentials: true // Enable credentials
 }));
 
 // Custom middleware to set additional CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4887');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
