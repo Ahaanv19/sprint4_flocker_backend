b49257e81fa8c207d769e0ef8457c744fd800c9f
jacobcancode <jake.zierolf@gmail.com>
2025-01-09 09:06:33 -0800

    fix again

--- api/ai.py
+++ api/ai.py
@@ -42,4 +42,4 @@ app.register_blueprint(ai_api)
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=4444)
