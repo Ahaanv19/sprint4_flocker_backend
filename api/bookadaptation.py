a4420e5a2a41ec2f5c22f023856a94c8d1d9c945
NoahH-10 <noah@sirkenneth.com>
2025-01-11 16:31:45 -0800

    Added Book Adaptations Checker API + data

---
+++ api/bookadaptation.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request
+import json
+import os
+
+bookadaptations = Flask(__name__)
+
+@bookadaptations.route('/', methods=['GET']) ##test if the server is running correctly
+def home():
+    return "Welcome to the Book Adaptations API!"
+
+def load_movies(): ##function to load the movies from the json file (movies.json)
+    try:
+        with open('movies.json') as f:
+            return json.load(f)
+    except FileNotFoundError: ##if the file is not found return an error message
+        return [], "File not found."
+    except json.JSONDecodeError: 
+        return [], "Error decoding JSON."
+    
+@bookadaptations.route('/movies', methods=['GET']) ##get all the movies
+def get_movies():
+    movies = load_movies()
+    return jsonify(movies)
+
+@bookadaptations.route('/movies/search', methods=['GET']) # run something like http://localhost:6000/movies/search?title=1984 to check in postman (it works)
+def search_movie():
+    title = request.args.get('title') ##get the title from the url
+    movies = load_movies()
+
+     # Find the movie by title (case-insensitive)
+    found_movie = next((movie for movie in movies if movie['title'].lower() == title.lower()), None)
+    
+    if found_movie: ##if the movie is found return it
+        return jsonify(found_movie) 
+    else:
+        return jsonify({'message': f'The movie "{title}" was not found.'}), 404
+
+if __name__ == '__main__': ##run the server on port 6000
+    bookadaptations.run(debug=True, port=6000)
