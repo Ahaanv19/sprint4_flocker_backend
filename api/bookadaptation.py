2f871d0c49f0fdbbe6c8bba37a9be3145e8c7bb6
NoahH-10 <noah@sirkenneth.com>
2025-01-11 17:26:26 -0800

    Updates

--- api/bookadaptation.py
+++ api/bookadaptation.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 import json
 import os
+from flask_cors import CORS
 
 bookadaptations = Flask(__name__)
+CORS(bookadaptations) ##to allow the frontend to access the API
+
 
 @bookadaptations.route('/', methods=['GET']) ##test if the server is running correctly
 def home():
@@ -22,18 +25,18 @@ def get_movies():
     movies = load_movies()
     return jsonify(movies)
 
-@bookadaptations.route('/movies/search', methods=['GET']) # run something like http://localhost:6000/movies/search?title=1984 to check in postman (it works)
+@bookadaptations.route('/movies/search', methods=['GET']) # run something like http://localhost:5000/movies/search?title=1984 to check in postman (it works)
 def search_movie():
     title = request.args.get('title') ##get the title from the url
     movies = load_movies()
 
      # Find the movie by title (case-insensitive)
-    found_movie = next((movie for movie in movies if movie['title'].lower() == title.lower()), None)
+    found_movie = next((movie for movie in movies if movie['title'].lower() == title.lower()), None) 
     
     if found_movie: ##if the movie is found return it
         return jsonify(found_movie) 
     else:
         return jsonify({'message': f'The movie "{title}" was not found.'}), 404
 
-if __name__ == '__main__': ##run the server on port 6000
-    bookadaptations.run(debug=True, port=6000)
+if __name__ == '__main__': ##run the server on port 5000
+    bookadaptations.run(debug=True, port=5000)
