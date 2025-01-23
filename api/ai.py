b9553cd8556f86db5acaa8313e0fe0e15a4b1874
jacobcancode <jake.zierolf@gmail.com>
2025-01-22 18:29:39 -0800

    chaNGES

--- api/ai.py
+++ api/ai.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 import json
 import os
 from flask_cors import CORS, cross_origin
+from model.reco import add_book_to_db, db
 
 ai_api = Blueprint('ai_api', __name__, url_prefix='/api')
 CORS(ai_api)  # to allow the frontend to access the API
@@ -20,6 +21,14 @@ def load_books():  # function to load the books from the json file (books.json)
     except json.JSONDecodeError:
         return {}, "Error decoding JSON."
 
+def save_books(books):  # function to save the books to the json file (books.json)
+    try:
+        with open('books.json', 'w') as f:
+            json.dump(books, f)
+        return None
+    except Exception as e:
+        return str(e)
+
 @ai_api.route('/recommendations', methods=['GET'])  # get all the books
 @cross_origin()
 def get_books():
@@ -37,8 +46,27 @@ def get_books():
     
     return jsonify(recommended_books)
 
+@ai_api.route('/books', methods=['POST'])  # add a new book
+@cross_origin()
+def add_book():
+    new_book = request.json
+    try:
+        added_book = add_book_to_db(new_book)
+        return jsonify({
+            'id': added_book.id,
+            'title': added_book.title,
+            'author': added_book.author,
+            'genre': added_book.genre
+        }), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     from flask import Flask
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.register_blueprint(ai_api)
-    app.run(debug=True, port=8887)
+    app.run(debug=True, port=5000)
--- model/reco.py
+++ model/reco.py
@@ -1,25 +1,19 @@
-from __init__ import db
+from flask_sqlalchemy import SQLAlchemy
 
-class Recommendation(db.Model):
+db = SQLAlchemy()
+
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     genre = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, title, author, genre):
-        self.title = title
-        self.author = author
-        self.genre = genre
-
-    def read(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'author': self.author,
-            'genre': self.genre
-        }
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
+def add_book_to_db(book_data):
+    new_book = Book(
+        title=book_data['title'],
+        author=book_data['author'],
+        genre=book_data['genre']
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return new_book
