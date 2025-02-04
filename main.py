dce1ba447e5775e6d8780e3a652c0842091620cd
Ahaan Vaidyanathan <ahaanvk@gmail.com>
2025-02-04 09:11:49 -0800

    Updates

--- main.py
+++ main.py
@@ -306,4 +306,4 @@ app.cli.add_command(custom_cli)
         
 # this runs the flask application on the development server
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8005)
+    app.run(debug=True, host="0.0.0.0", port=8103)
