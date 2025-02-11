548efb73cd2146dbd73b23fb01de600549100af1
NoahH-10 <noah@sirkenneth.com>
2025-02-11 09:17:39 -0800

    CORS and fetch errors potential fix

--- server.js
+++ server.js
@@ -6,18 +6,9 @@ const port = 8103;
 
 // Use the CORS middleware to allow requests from your frontend
 app.use(cors({
-  origin: 'http://127.0.0.1:4887', // Specify the exact origin of your frontend
+  origin: 'https://ahaanv19.github.io', // Specify the exact origin of your frontend
   credentials: true // Enable credentials
 }));
-
-// Custom middleware to set additional CORS headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4887');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
