8f1b07e420563e8f872d84cde222a0c5ba5bf101
Ahaan Vaidyanathan <ahaanvk@gmail.com>
2025-02-12 12:05:01 -0800

    Fixed bad gatway

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const port = 8103;
 
 // Use the CORS middleware to allow requests from your frontend
 app.use(cors({
-  origin: ['https://ahaanv19.github.io', 'https://ahaanv19.github.io/'], // Allow both origins
+  origin: 'https://ahaanv19.github.io', // Replace with your actual frontend URL
   credentials: true // Enable credentials
 }));
 
 // Custom middleware to set additional CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ahaanv19.github.io'); // Adjust as necessary
+  res.setHeader('Access-Control-Allow-Origin', 'https://ahaanv19.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
