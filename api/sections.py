985336ff34ab1030322aa14f3689b9b2b7cc63a3
Ahaan Vaidyanathan <ahaanvk@gmail.com>
2025-02-12 11:51:33 -0800

    Updates

--- api/sections.py
+++ api/sections.py
@@ -1,20 +1,23 @@
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
+from flask_cors import CORS  # Import CORS
 from model.mod import Section
 
 section_api = Blueprint('section_api', __name__, url_prefix='/api')
+CORS(section_api, resources={r"/*": {"origins": "https://ahaanv19.github.io"}})  # Apply CORS
+
 api = Api(section_api)
 
 class SectionAPI:
     class _CRUD(Resource):
         def post(self):
-            data = request.get_json()  # Get data from frontend
+            data = request.get_json()
             section = Section(data['name'], data['theme'])
-            section.create()  # Add section to the DB
+            section.create()
             return jsonify(section.read())
 
         def get(self):
-            sections = Section.query.all()  # Get all sections from the DB
+            sections = Section.query.all()
             return jsonify([section.read() for section in sections])
 
         def put(self):
